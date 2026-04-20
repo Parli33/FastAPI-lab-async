cbd40dfb074c2b2b4b15a745e318c50578675c33
parli <theili9a@gmail.com>
2026-04-20 10:24:52 +0700

    feat: добавлен WebSocket для real-time статуса задачи

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
 from app.config import settings
-from app.routers import tasks_router
+from app.routers import tasks_router, websocket_router
 
 app = FastAPI(title=settings.app_name)
 app.include_router(tasks_router)
+app.include_router(websocket_router)
 @app.get("/")
 async def root():
     return {"message": "Hello, Async FastAPI!"}
